4fcc2436ba40cb7ed9d7096735ddb0aafae8a94b
myworkmail4-png <myworkmail4@gmail.com>
2026-01-11 08:07:04 +0100

    fix(task-19): Add tier1_score_breakdown for imported records

---
+++ scripts/update-flatchr-breakdown.sql
@@ -0,0 +1,175 @@
+-- Update tier1_score_breakdown for 43 imported records
+-- Generated: 2026-01-11T07:06:56.079Z
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":3,"q5_score":10,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":10,"q15_score":3,"q16_score":2,"q17_score":0,"total":44}'::jsonb
+WHERE flatchr_id = 'KJ0Mpzw144WdXNgx';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":4,"q9_score":0,"q10_score":6,"q11_score":2,"q12_score":10,"q14_score":6,"q15_score":3,"q16_score":2,"q17_score":0,"total":68}'::jsonb
+WHERE flatchr_id = '41QG9eO61XBdK6Xe';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":4,"q9_score":10,"q10_score":10,"q11_score":2,"q12_score":10,"q14_score":6,"q15_score":3,"q16_score":2,"q17_score":0,"total":79}'::jsonb
+WHERE flatchr_id = 'EZBvp5zZobvdMoVm';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":4,"q9_score":0,"q10_score":6,"q11_score":2,"q12_score":10,"q14_score":6,"q15_score":3,"q16_score":2,"q17_score":0,"total":68}'::jsonb
+WHERE flatchr_id = 'mQNJ9W07jzwp5LwD';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":4,"q9_score":10,"q10_score":10,"q11_score":2,"q12_score":10,"q14_score":6,"q15_score":3,"q16_score":2,"q17_score":0,"total":79}'::jsonb
+WHERE flatchr_id = 'Qayv9LLODbA96LXE';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":10,"q3_score":3,"q5_score":0,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":10,"q14_score":4,"q15_score":1,"q16_score":0,"q17_score":5,"total":46}'::jsonb
+WHERE flatchr_id = '8oWlpG8OXROd63O1';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":5,"q3_score":3,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":1,"q16_score":0,"q17_score":0,"total":39}'::jsonb
+WHERE flatchr_id = '56Rb9AbZWROpoNZQ';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":0,"q3_score":5,"q5_score":10,"q6_score":10,"q8_score":4,"q9_score":0,"q10_score":6,"q11_score":5,"q12_score":10,"q14_score":2,"q15_score":1,"q16_score":0,"q17_score":0,"total":63}'::jsonb
+WHERE flatchr_id = 'mQNJ9W07yDjp5LwD';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":7.5,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":0,"q10_score":6,"q11_score":2,"q12_score":10,"q14_score":2,"q15_score":3,"q16_score":2,"q17_score":5,"total":64.5}'::jsonb
+WHERE flatchr_id = 'KzqPnNjgL70n31m0';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":10,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":2,"q17_score":0,"total":51}'::jsonb
+WHERE flatchr_id = 'xy5JpxrmyRBnz7lV';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":7.5,"q3_score":3,"q5_score":0,"q6_score":10,"q8_score":6,"q9_score":10,"q10_score":10,"q11_score":2,"q12_score":10,"q14_score":4,"q15_score":3,"q16_score":2,"q17_score":0,"total":72.5}'::jsonb
+WHERE flatchr_id = 'Bely9PADAmEpkEKR';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":7.5,"q3_score":3,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":10,"q14_score":2,"q15_score":1,"q16_score":2,"q17_score":0,"total":58.5}'::jsonb
+WHERE flatchr_id = 'JL2lpE0gYNl9wBXK';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":5,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":4,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":1,"q16_score":0,"q17_score":0,"total":50}'::jsonb
+WHERE flatchr_id = 'V4A8d008Va2dKxjR';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":7.5,"q3_score":5,"q5_score":10,"q6_score":10,"q8_score":4,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":2,"q15_score":3,"q16_score":0,"q17_score":0,"total":60.5}'::jsonb
+WHERE flatchr_id = '8oWlpG8gQL0d63O1';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":6,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":10,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":56}'::jsonb
+WHERE flatchr_id = 'Bely9PADkAbpkEKR';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":10,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":5,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":55}'::jsonb
+WHERE flatchr_id = 'bMGYdVgQam1n3JPN';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":7.5,"q3_score":5,"q5_score":3,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":8,"q11_score":5,"q12_score":10,"q14_score":6,"q15_score":1,"q16_score":2,"q17_score":5,"total":64.5}'::jsonb
+WHERE flatchr_id = '5aMxpQA2VkVpGZ2O';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":7.5,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":6,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":50.5}'::jsonb
+WHERE flatchr_id = '8q3Bp4lGW6lp0RMg';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":5,"q3_score":5,"q5_score":3,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":4,"q11_score":5,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":32}'::jsonb
+WHERE flatchr_id = '7gWQnjjlYl6nJRak';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":0,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":3,"q16_score":0,"q17_score":0,"total":43}'::jsonb
+WHERE flatchr_id = 'Qayv9LLgAMo96LXE';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":3,"q5_score":10,"q6_score":0,"q8_score":4,"q9_score":0,"q10_score":10,"q11_score":2,"q12_score":10,"q14_score":8,"q15_score":3,"q16_score":2,"q17_score":5,"total":77}'::jsonb
+WHERE flatchr_id = '7gWQnjjl6NgnJRak';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":6,"q11_score":5,"q12_score":0,"q14_score":4,"q15_score":1,"q16_score":0,"q17_score":0,"total":48}'::jsonb
+WHERE flatchr_id = 'yObP9OgxjZ291ZYB';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":0,"q3_score":3,"q5_score":7.5,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":22.5}'::jsonb
+WHERE flatchr_id = 'q1xlpRAYMD496zEM';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":0,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":0,"q10_score":10,"q11_score":5,"q12_score":10,"q14_score":4,"q15_score":1,"q16_score":4,"q17_score":5,"total":61}'::jsonb
+WHERE flatchr_id = '56Rb9AbgegwpoNZQ';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":0,"q3_score":3,"q5_score":5,"q6_score":10,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":3,"q16_score":2,"q17_score":5,"total":48}'::jsonb
+WHERE flatchr_id = '8q3Bp4lGY4Op0RMg';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":0,"q3_score":3,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":0,"q15_score":3,"q16_score":0,"q17_score":0,"total":42}'::jsonb
+WHERE flatchr_id = 'DYjo9m2707jpkR0r';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":10,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":4,"q9_score":0,"q10_score":2,"q11_score":5,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":44}'::jsonb
+WHERE flatchr_id = 'YJA39J4gL73dlNQw';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":3,"q5_score":10,"q6_score":10,"q8_score":8,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":3,"q16_score":0,"q17_score":0,"total":62}'::jsonb
+WHERE flatchr_id = 'yVgL9kxvx2Bp1PD5';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":3,"q2_score":10,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":6,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":49}'::jsonb
+WHERE flatchr_id = 'brjV9MeRLaB9R5mN';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":5,"q3_score":5,"q5_score":0,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":2,"q15_score":1,"q16_score":0,"q17_score":0,"total":26}'::jsonb
+WHERE flatchr_id = '5kDWnb58NLydyawr';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":3,"q5_score":3,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":2,"q15_score":3,"q16_score":0,"q17_score":0,"total":27}'::jsonb
+WHERE flatchr_id = 'bMGYdVgRBB1n3JPN';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":3,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":10,"q14_score":2,"q15_score":1,"q16_score":2,"q17_score":0,"total":54}'::jsonb
+WHERE flatchr_id = 'YXmVp3VKG1m98Mzr';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":0,"q3_score":3,"q5_score":5,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":10,"q14_score":0,"q15_score":1,"q16_score":2,"q17_score":5,"total":59}'::jsonb
+WHERE flatchr_id = 'y3EOp20EQgon1KMq';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":3,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":5,"q12_score":0,"q14_score":10,"q15_score":1,"q16_score":0,"q17_score":0,"total":68}'::jsonb
+WHERE flatchr_id = '41QG9eO44M8dK6Xe';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":0,"q3_score":5,"q5_score":3,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":4,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":3,"q16_score":0,"q17_score":0,"total":30}'::jsonb
+WHERE flatchr_id = '67MknKLR35E9J120';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":3,"q5_score":0,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":2,"q15_score":1,"q16_score":0,"q17_score":5,"total":27}'::jsonb
+WHERE flatchr_id = '8q3Bp4lNDgap0RMg';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":5,"q5_score":0,"q6_score":0,"q8_score":4,"q9_score":10,"q10_score":2,"q11_score":5,"q12_score":0,"q14_score":0,"q15_score":1,"q16_score":0,"q17_score":0,"total":37}'::jsonb
+WHERE flatchr_id = 'yVgL9kxvXDzp1PD5';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":7,"q2_score":0,"q3_score":3,"q5_score":0,"q6_score":10,"q8_score":2,"q9_score":10,"q10_score":2,"q11_score":2,"q12_score":0,"q14_score":4,"q15_score":3,"q16_score":0,"q17_score":0,"total":43}'::jsonb
+WHERE flatchr_id = '8oWlpG8NBM5d63O1';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":6,"q9_score":0,"q10_score":2,"q11_score":5,"q12_score":0,"q14_score":0,"q15_score":3,"q16_score":0,"q17_score":0,"total":46}'::jsonb
+WHERE flatchr_id = 'MQj09ZrRmOw9xoRL';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":5,"q5_score":0,"q6_score":10,"q8_score":4,"q9_score":10,"q10_score":8,"q11_score":2,"q12_score":10,"q14_score":10,"q15_score":3,"q16_score":0,"q17_score":0,"total":82}'::jsonb
+WHERE flatchr_id = 'xy5Jpxrw8ornz7lV';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":10,"q8_score":6,"q9_score":10,"q10_score":4,"q11_score":2,"q12_score":10,"q14_score":6,"q15_score":3,"q16_score":0,"q17_score":5,"total":91}'::jsonb
+WHERE flatchr_id = 'lV0D9aLD6KVnWELk';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":10,"q2_score":10,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":2,"q9_score":10,"q10_score":6,"q11_score":2,"q12_score":10,"q14_score":10,"q15_score":3,"q16_score":2,"q17_score":0,"total":80}'::jsonb
+WHERE flatchr_id = '5kDWnb5YqWMdyawr';
+
+UPDATE repreneurs
+SET tier1_score_breakdown = '{"q1_score":5,"q2_score":5,"q3_score":5,"q5_score":10,"q6_score":0,"q8_score":2,"q9_score":0,"q10_score":8,"q11_score":5,"q12_score":10,"q14_score":2,"q15_score":3,"q16_score":0,"q17_score":5,"total":60}'::jsonb
+WHERE flatchr_id = 'YXmVp3Vjb0B98Mzr';
+
